6a82a1fc4fce3e8ae3ae020e89ab5889c06fe242
Douglas <166155925+unknowDgK@users.noreply.github.com>
2026-07-25 14:52:40 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'controle-validade-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // network-first para permitir dados sempre atualizados; cache como fallback offline
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
